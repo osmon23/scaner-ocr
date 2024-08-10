72a7a0c29987c81dd0a3744d0903ae87c11737a4
osmon23 <oma_23_03_98@mail.ru>
2024-08-10 22:42:41 +0600

    static refactor

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR/'static'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets')
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
